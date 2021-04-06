d156b070c6602211a5992cf9e9ef083415f278d6
unknown <ronaldpsantos@outlook.com>
2021-04-06 12:36:39 -0300

    Other Migrations

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int id { get; set; }
         public string name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void AddSeler(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
+
     }
 }
