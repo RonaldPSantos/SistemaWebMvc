47680bdd4a5691bcdc870e2928abc36b5b506670
unknown <ronaldpsantos@outlook.com>
2021-04-06 09:48:23 -0300

    First commit trainning

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["Professor"] = "Nélio alves";
             return View();
         }
 
